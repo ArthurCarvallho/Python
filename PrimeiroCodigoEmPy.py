f904149d82ff32c4533b11734f8646b1fbe89f6d
Arthur <arthurpdc07@gmail.com>
2025-05-04 11:22:52 -0300

    Primeiro teste de Python

---
+++ PrimeiroCodigoEmPy.py
@@ -0,0 +1,9 @@
+print("informações pessoais:");
+
+nome = input("Digite seu nome:");
+idade  = input("Digite sua idade:");
+email = input("Digite seu email:");
+
+print("Nome:", nome);
+print("Idade:", idade);
+print("Email:", email);
